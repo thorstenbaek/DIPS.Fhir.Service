e8921fa60daa8b35d5b6d498e704562aa473e5e5
Thor Stenbæk <tst@dips.no>
2022-06-02 22:39:03 +0200

    new PC

---
+++ src/DIPS.Fhir.Service/Entities/BinaryMapping.cs
@@ -0,0 +1,21 @@
+﻿using NHibernate.Extensions.NpgSql;
+using NHibernate.Mapping.ByCode.Conformist;
+
+namespace DIPS.Fhir.Service.Entities
+{
+    public class BinaryMapping : ClassMapping<BinaryEntity>
+    {
+        public BinaryMapping()
+        {
+            Id(x => x.Id);
+            Schema("public");
+            Table("binary");
+            Property(
+                p => p.Resource,
+                m =>
+                {
+                    m.Type<JsonType>();
+                });
+        }
+    }
+}
